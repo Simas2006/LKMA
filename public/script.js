79145c530e1902dde9e3ca15a8b849341c6fa99a
Simas2006 <sjuknelis@gmail.com>
2019-07-29 14:36:44 -0400

    Add animations to carousel

--- public/script.js
+++ public/script.js
@@ -1,24 +1,43 @@
 var carousel;
+var allowMove = true;
 
-function renderCarousel() {
-  carousel.element.style.backgroundImage = `url("/images/${carousel.images[carousel.index]}")`;
-}
-
-function getCarousel() {
+function initCarousel() {
   var element = document.getElementsByClassName("carousel")[0];
+  if ( ! element ) return;
+  var images = element.getAttribute("data-images").split(" ");
+  var displays = [];
+  for ( var i = 0; i < images.length; i++ ) {
+    var div = document.createElement("div");
+    div.className = "carousel-display";
+    div.style.backgroundImage = `url("/images/${images[i]}")`;
+    element.appendChild(div);
+    displays.push(div);
+  }
+  displays[0].style.display = "block";
   carousel = {
-    "element": element,
-    "images": element.getAttribute("data-images").split(" "),
+    "displays": displays,
     "index": 0
   }
-  renderCarousel();
 }
 
 function moveCarousel(move) {
+  if ( ! allowMove ) return;
+  allowMove = false;
+  var lastIndex = carousel.index;
   carousel.index += move;
-  if ( carousel.index <= -1 ) carousel.index = carousel.index + carousel.images.length;
-  if ( carousel.index >= carousel.images.length ) carousel.index = carousel.index - carousel.images.length;
-  renderCarousel();
+  if ( carousel.index <= -1 ) carousel.index = carousel.index + carousel.displays.length;
+  if ( carousel.index >= carousel.displays.length ) carousel.index = carousel.index - carousel.displays.length;
+  var direction = move < 0 ? "Left" : "Right";
+  carousel.displays[carousel.index].style.display = "block";
+  carousel.displays[lastIndex].classList.add("exit" + direction);
+  carousel.displays[carousel.index].classList.add("enter" + direction);
+  setTimeout(function() {
+    carousel.displays[lastIndex].classList.remove("exit" + direction);
+    carousel.displays[carousel.index].classList.remove("enter" + direction);
+    carousel.displays[lastIndex].style.display = "none";
+    carousel.displays[carousel.index].style.display = "block";
+    allowMove = true;
+  },1000);
 }
 
-window.onload = getCarousel;
+window.onload = initCarousel;
