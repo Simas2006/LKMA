f570ba4c730288337549b2c77dbd6ea7cead1ea0
Simas2006 <sjuknelis@gmail.com>
2019-07-29 11:28:55 -0400

    Make page_view script functional

--- public/page_view.php
+++ public/page_view.php
@@ -2,6 +2,17 @@
 $page = $_GET["page"];
 $path = "../templates/" . $page . ".html";
 $obj = @fopen($path,"r") or die("Bad Request");
-echo fread($obj,filesize($path));
+$data = fread($obj,filesize($path));
 fclose($obj);
+
+$path = "../templates/translations/" . $_GET["file"] . ".json";
+$obj = @fopen($path,"r") or die("Bad Request");
+$translationData = json_decode(fread($obj,filesize($path))) -> {$page};
+fclose($obj);
+$keys = array_keys((array) $translationData);
+
+foreach ( $keys as $value ) {
+  $data = str_replace("{" . $value . "}",$translationData -> {$value},$data);
+}
+echo $data;
 ?>
